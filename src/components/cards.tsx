3a9f46dbb9088bc7d5af9d6e8686ab8176177db7
Josue Monteiro <josue.player01@gmail.com>
2024-04-14 13:44:14 -0300

    "Add Myname and Cards components to Home page"
    This commit adds two new components, Myname and Cards, to the Home page. The Myname component displays the text "JOSUE UCHOA" and the Cards component show a list of cards

---
+++ src/components/cards.tsx
@@ -0,0 +1,56 @@
+"use client";
+import {useEffect, useRef} from "react";
+
+export default function Cards() {
+	const handleOnMouseMove = (e) => {
+		const cards = document.querySelectorAll(".cards");
+		cards.forEach((card) => {
+			const rect = card.getBoundingClientRect(),
+				x = e.clientX - rect.left,
+				y = e.clientY - rect.top;
+			card.style.setProperty("--mouse-x", `${x}px`);
+			card.style.setProperty("--mouse-y", `${y}px`);
+		});
+	};
+	useEffect(() => {
+		document
+			.getElementById("cards")
+			?.addEventListener("mousemove", (e: MouseEvent) => {
+				const cards = document.getElementsByClassName(
+					"card"
+				) as HTMLCollectionOf<HTMLElement>;
+				for (const card of Array.from(cards)) {
+					const rect = card.getBoundingClientRect(),
+						x = e.clientX - rect.left,
+						y = e.clientY - rect.top;
+
+					card.style.setProperty("--mouse-x", `${x}px`);
+					card.style.setProperty("--mouse-y", `${y}px`);
+				}
+			});
+	});
+
+	return (
+		<div
+			id="cards"
+			className="flex row"
+		>
+			<div className="card">
+				<div className="card-border"></div>
+				<div className="card-content"></div>
+			</div>
+			<div className="card">
+				<div className="card-border"></div>
+				<div className="card-content"></div>
+			</div>
+			<div className="card">
+				<div className="card-border"></div>
+				<div className="card-content"></div>
+			</div>
+			<div className="card">
+				<div className="card-border"></div>
+				<div className="card-content"></div>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/myname.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {useEffect} from "react";
+
+interface propiertis {
+	word: String;
+}
+export default function Myname(props: propiertis) {
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	function changeText(event) {
+		let interation = 0;
+		const interval = setInterval(() => {
+			event.target.innerText = event.target.innerText
+				.split("")
+				.map((letter: String, index: number) => {
+					if (index < interation) {
+						return props.word[index];
+					} else {
+						return letters[Math.floor(Math.random() * 26)];
+					}
+				})
+				.join("");
+
+			if (interation >= props.word.length) clearInterval(interval);
+
+			interation += 1 / 3;
+		}, 30);
+	}
+
+	return (
+		<div className="text-9xl m-48 font-mono font-semibold">
+			<a
+				className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+				href="https://github.com/JosueMonteiroUchoaAlves"
+				target="_blank"
+				rel="noopener noreferrer"
+				onMouseEnter={(event) => changeText(event)}
+			>
+				JOSUE UCHOA
+			</a>
+		</div>
+	);
+}
